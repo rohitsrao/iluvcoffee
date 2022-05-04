4707ed9b014bf6d262c0e0837eb2740f69759fda
Rohit Rao <rohit@rohitsrao.com>
2022-05-04 19:52:12 +0200

    Corrected forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    forbidUnknownValues: true,
+    forbidNonWhitelisted: true,
     whitelist: true,
   }))
   await app.listen(3000);
